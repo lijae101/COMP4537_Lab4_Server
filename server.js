f49de9166e9745bddc672a4e59c3c2d3134545a0
Saba karbakhsh <skarbakhshravari@my.bcit.ca>
2025-02-13 08:31:32 -0800

    .

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const url = require('url');
 
 let messages = require('./messages.js');
 let dictionary = {};
-
+let getreq =0;
+let postreq =0;
 
 class Server {
     constructor() {
@@ -20,12 +21,16 @@ class Server {
             if (q.pathname === '/api/definitions') {
                 if (req.method === 'GET') {
                     let word = q.query.word;
+                    console.log(dictionary);
                     if ( dictionary[word] === undefined) {
                         res.writeHead(404, { 'Content-Type': 'application/json' });
                         res.end(JSON.stringify({ message: messages.userMessages.wordNotFound }));
                     } else {
+                        getreq++;
                         res.writeHead(200, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify({ word: word, definition: dictionary[word] }));
+                        console.log("dictionary");
+                        console.log(getreq);
+                        res.end(JSON.stringify({ word: word, definition: dictionary[word], reqNum : getreq }));
                     }
 
 
@@ -42,14 +47,17 @@ class Server {
                         let definition =q['definition'];
                         let resualt = '';
                         if (word != null && definition != null && dictionary[word] === undefined) {
+                            postreq++;
                             dictionary[word] = definition;
                             res.writeHead(200, { 'Content-Type': 'text/plain' });
                             resualt= messages.userMessages.wordAdded;
+                            console.log("dictionary");
+                            console.log(getreq);
                         } else {
                             res.writeHead(201, { 'Content-Type': 'text/plain' });
                             resualt = messages.userMessages.wordExist;
                         }
-                        res.end(JSON.stringify({ message: resualt }));
+                        res.end(JSON.stringify({ message: resualt , reqNum : postreq}));
                     });
                 }
 
